842e79b8ae081b4518f1a1f59c99b252bf73413d
Thavas Antonio <66140321+BuddyBob@users.noreply.github.com>
2021-07-23 14:23:07 -0700

    changed from dev firebase to prod firebase

--- src/config.js
+++ src/config.js
@@ -1,11 +1,20 @@
 
+// var config = {
+//     "REACT_APP_FIREBASE_KEY":"AIzaSyA7JEfrmQYYJ8zPTDBvIiA-2Ma6yqOzYNY",
+//     "REACT_APP_FIREBASE_DOMAIN":"quick-type-dev.firebaseapp.com",
+//     "REACT_APP_FIREBASE_DATABASE":"https://quick-type-dev-default-rtdb.firebaseio.com",
+//     "REACT_APP_FIREBASE_PROJECT_ID":"quick-type-dev",
+//     "REACT_APP_FIREBASE_STORAGE_BUCKET":"quick-type-dev.appspot.com",
+//     "REACT_APP_FIREBASE_SENDER_ID":"852960131480",
+//     "REACT_APP_MESSAGING_APP_ID":"1:852960131480:web:fbb7ad852d17b596bf13ed"
+// }
 var config = {
-    "REACT_APP_FIREBASE_KEY":"AIzaSyA7JEfrmQYYJ8zPTDBvIiA-2Ma6yqOzYNY",
-    "REACT_APP_FIREBASE_DOMAIN":"quick-type-dev.firebaseapp.com",
+    "REACT_APP_FIREBASE_KEY":"AIzaSyAl1wDvVihUhDWavn9FpKfbKbO2yifOTJg",
+    "REACT_APP_FIREBASE_DOMAIN":"quicktype-prod-f5892.firebaseapp.com",
     "REACT_APP_FIREBASE_DATABASE":"https://quick-type-dev-default-rtdb.firebaseio.com",
-    "REACT_APP_FIREBASE_PROJECT_ID":"quick-type-dev",
-    "REACT_APP_FIREBASE_STORAGE_BUCKET":"quick-type-dev.appspot.com",
-    "REACT_APP_FIREBASE_SENDER_ID":"852960131480",
-    "REACT_APP_MESSAGING_APP_ID":"1:852960131480:web:fbb7ad852d17b596bf13ed"
+    "REACT_APP_FIREBASE_PROJECT_ID":"quicktype-prod-f5892",
+    "REACT_APP_FIREBASE_STORAGE_BUCKET":"quicktype-prod-f5892.appspot.com",
+    "REACT_APP_FIREBASE_SENDER_ID":"89248312961",
+    "REACT_APP_MESSAGING_APP_ID":"1:89248312961:web:a524f86db4090a95696780"
 }
 export default config

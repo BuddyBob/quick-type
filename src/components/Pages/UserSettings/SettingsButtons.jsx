3657cfa313af04633b97fed98944d0ada582e3a0
Thavas Antonio <66140321+BuddyBob@users.noreply.github.com>
2021-07-28 12:29:37 -0700

    removed test class 'wrrdd'

--- src/components/Pages/UserSettings/SettingsButtons.jsx
+++ src/components/Pages/UserSettings/SettingsButtons.jsx
@@ -98,7 +98,7 @@ const SettingsButtons = ({dbData,change,types}) => {
             </form>
         }
         {change === "wordCount" &&
-            <form className="wrrdd">
+            <form>
                 <div className="row">
                     <div className="col mt-3 wordCount-label">
                         <Label type="label" >{change}</Label>

4e68df27307d8775692be71a886d13b974feb91d
BuddyBob <66140321+BuddyBob@users.noreply.github.com>
2021-08-20 11:12:21 -0700

    initial

---
+++ build/precache-manifest.53436926f306392bf8a60929b5cd791d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "923d8b8db8a072454971",
+    "url": "/static/css/main.191264c7.chunk.css"
+  },
+  {
+    "revision": "923d8b8db8a072454971",
+    "url": "/static/js/main.f7cebb76.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "70cb10bc320d73d3cda7",
+    "url": "/static/js/2.6af7cf52.chunk.js"
+  },
+  {
+    "revision": "bdbe67a31f481111e830b3ba390a6e6e",
+    "url": "/static/media/Cheetah.bdbe67a3.png"
+  },
+  {
+    "revision": "37431d57e105cc71ce4f23a2b5c6bd2a",
+    "url": "/static/media/Sloth.37431d57.png"
+  },
+  {
+    "revision": "a6b5a97963a293cb31928602ebab2d34",
+    "url": "/static/media/Horse.a6b5a979.png"
+  },
+  {
+    "revision": "4d606826eed916e8c186c5cda1510a7b",
+    "url": "/static/media/Elephant.4d606826.png"
+  },
+  {
+    "revision": "614749c8d93690ecf737d5b16b6ed3fa",
+    "url": "/static/media/Hacker.614749c8.png"
+  },
+  {
+    "revision": "2d5b60d2220640972b365002918f1f50",
+    "url": "/static/media/click1.2d5b60d2.wav"
+  },
+  {
+    "revision": "9f8bceb6b5482c403ee251f4cd1faf93",
+    "url": "/static/media/click2.9f8bceb6.wav"
+  },
+  {
+    "revision": "c34ccd873cee7e3f003cdee6ac704b36",
+    "url": "/static/media/click3.c34ccd87.wav"
+  },
+  {
+    "revision": "2d5b60d2220640972b365002918f1f50",
+    "url": "/static/media/click4.2d5b60d2.wav"
+  },
+  {
+    "revision": "9f8bceb6b5482c403ee251f4cd1faf93",
+    "url": "/static/media/click5.9f8bceb6.wav"
+  },
+  {
+    "revision": "c34ccd873cee7e3f003cdee6ac704b36",
+    "url": "/static/media/click6.c34ccd87.wav"
+  },
+  {
+    "revision": "223e685610df31a4c297021ef7e6f7c2",
+    "url": "/static/media/click7.223e6856.wav"
+  },
+  {
+    "revision": "1fc92df9a9fa384574eb95e97b575d5e",
+    "url": "/index.html"
+  }
+];
